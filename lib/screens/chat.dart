d2cd2b18bc8d25e4d16cc17148f0de1a5797ed4c
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-03-21 16:09:38 +0100

    Add project files

---
+++ lib/screens/chat.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    // final token = await fcm.getToken(); -> you can send this token (via HTTP or Firestore SDK) to the backend
+
+    fcm.subscribeToTopic('myChat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications(); // because inside initState() method we SHOULD'T use async/await, so we pass it as a separate method
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
